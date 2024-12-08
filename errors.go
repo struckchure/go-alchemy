9bd095183fd50b0d525ca479b27271f120f8bbe0
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2024-12-08 12:20:38 +0100

    feat(errors.go): adds config not found error

--- errors.go
+++ errors.go
@@ -2,6 +2,4 @@ package alchemy
 
 import "errors"
 
-var ErrInvalidCredentials error = errors.New("invalid credentials")
-var ErrNotFound error = errors.New("record not found")
-var ErrDuplicateRecord error = errors.New("record already exist")
+var ErrAlchemyConfigNotFound error = errors.New("alchemy config not found")
