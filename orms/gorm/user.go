32892a67b501c05b41239cb5d2e1eff494527a5b
Olakotan Daniel <officialhorlakz@gmail.com>
2024-12-10 17:40:51 +0100

    feat(orms/gorm): implement user CRUD operations and payload structures

--- orms/gorm/user.go
+++ orms/gorm/user.go
@@ -1,7 +1,18 @@
 package gorm
 
+import "gorm.io/gorm"
+
 type User struct {
 	Id        string `json:"id"`
+	FirstName string `json:"firstName" gorm:"column:first_name"`
+	LastName  string `json:"lastName" gorm:"column:last_name"`
+	Email     string `json:"email"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"created_at" gorm:"column:created_at"`
+}
+
+type UserCreatePayload struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	Email     string `json:"email"`
@@ -9,50 +20,86 @@ type User struct {
 	Password  string `json:"password"`
 }
 
-type UserCreatePayload struct{}
-
-type UserUpdatePayload struct{}
+type UserUpdatePayload struct {
+	Id        *string `json:"id"`
+	FirstName *string `json:"firstName"`
+	LastName  *string `json:"lastName"`
+	Email     *string `json:"email"`
+	Username  *string `json:"username"`
+	Password  *string `json:"password"`
+}
 
 type IUserDao interface {
 	List() ([]User, error)
-	Get(string) (*User, error)
-	GetByEmail(string) (*User, error)
-	GetByUsername(string) (*User, error)
+	Get(string) (User, error)
+	GetByEmail(string) (User, error)
+	GetByUsername(string) (User, error)
 	Create(UserCreatePayload) (*User, error)
 	Update(UserUpdatePayload) (*User, error)
 	Delete(string) error
 }
 
-type UserDao struct{}
-
-func (u *UserDao) List() ([]User, error) {
-	panic("unimplemented")
+type UserDao struct {
+	client *gorm.DB
 }
 
-func (u *UserDao) Get(id string) (*User, error) {
-	panic("unimplemented")
+func (u *UserDao) List() (users []User, err error) {
+	err = u.client.Model(&User{}).Find(&users).Error
+
+	return users, err
 }
 
-func (u *UserDao) GetByEmail(email string) (*User, error) {
-	panic("unimplemented")
+func (u *UserDao) Get(id string) (user User, err error) {
+	err = u.client.Model(&User{}).Where("id = ?", id).First(&user).Error
+
+	return user, err
 }
 
-func (u *UserDao) GetByUsername(username string) (*User, error) {
-	panic("unimplemented")
+func (u *UserDao) GetByEmail(email string) (user User, err error) {
+	err = u.client.Model(&User{}).Where("email = ?", email).First(&user).Error
+
+	return user, err
+}
+
+func (u *UserDao) GetByUsername(username string) (user User, err error) {
+	err = u.client.Model(&User{}).Where("username = ?", username).First(&user).Error
+
+	return user, err
 }
 
 func (u *UserDao) Create(payload UserCreatePayload) (*User, error) {
-	panic("unimplemented")
+	user := User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Username:  payload.Username,
+		Password:  payload.Password,
+	}
+
+	err := u.client.Create(&user).Error
+
+	return &user, err
 }
 
 func (u *UserDao) Update(payload UserUpdatePayload) (*User, error) {
-	panic("unimplemented")
+	user := User{
+		Id:        *payload.Id,
+		FirstName: *payload.FirstName,
+		LastName:  *payload.LastName,
+		Email:     *payload.Email,
+		Username:  *payload.Username,
+		Password:  *payload.Password,
+	}
+
+	err := u.client.Model(&User{}).Where("id = ?", user.Id).Updates(&user).Error
+
+	return &user, err
 }
 
 func (u *UserDao) Delete(id string) error {
-	panic("unimplemented")
+	return u.client.Where("id = ?", id).Delete(&User{}).Error
 }
 
-func NewUserDao() IUserDao {
-	return &UserDao{}
+func NewUserDao(client *gorm.DB) IUserDao {
+	return &UserDao{client: client}
 }
